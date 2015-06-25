c45d56c131251820d722e354d74536fd7813a03c
clairtonluz <clairton.c.l@gmail.com>
2015-06-25 19:01:27 -0300

    refactoring services

--- app/routes/hosts.js
+++ app/routes/hosts.js
@@ -1,37 +1,37 @@
 module.exports = function(app) {
-	app.get('/hosts', getHosts);
-	
-	function getHosts(req, res) {
-	  var _hosts = [{
-		 title: "bytecom-mk-patricia-gomes",
-		 ip: "10.77.100.1",
-		 nodes: [
-				{
-				  title: "bytecom-patricia-gomes-rep1",
-				  ip: "10.77.100.101",
-				  nodes: []
-				}, {
-				  title: "bytecom-patricia-gomes-rep2",
-				  ip: "10.77.10100.102",
-				  nodes: []
-				}, {
-				  title: "bytecom-patricia-gomes-rep3",
-				  ip: "10.77.100.103",
-				  nodes: []
-				}, {
-				  title: "bytecom-patricia-gomes-rep4",
-				  ip: "10.77.100100.104",
-				  nodes: []
-				}, {
-				  title: "bytecom-patricia-gomes-omni",
-				  ip: "10.77.100.105",
-				  nodes: []
-				}]
-		 }];
+	app.get('/hosts', getHosts);	
+}
 
-		var _json = JSON.stringify(_hosts);
+function getHosts(req, res) {
+  var _hosts = [{
+	 title: "bytecom-mk-patricia-gomes",
+	 ip: "10.77.100.1",
+	 nodes: [
+			{
+			  title: "bytecom-patricia-gomes-rep1",
+			  ip: "10.77.100.101",
+			  nodes: []
+			}, {
+			  title: "bytecom-patricia-gomes-rep2",
+			  ip: "10.77.10100.102",
+			  nodes: []
+			}, {
+			  title: "bytecom-patricia-gomes-rep3",
+			  ip: "10.77.100.103",
+			  nodes: []
+			}, {
+			  title: "bytecom-patricia-gomes-rep4",
+			  ip: "10.77.100100.104",
+			  nodes: []
+			}, {
+			  title: "bytecom-patricia-gomes-omni",
+			  ip: "10.77.100.105",
+			  nodes: []
+			}]
+	 }];
 
-		res.set('Content-Type', 'application/json');
-		res.end(_json);
-	}
+	var _json = JSON.stringify(_hosts);
+
+	res.set('Content-Type', 'application/json');
+	res.end(_json);
 }
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var ping = require ("net-ping");
-var session = ping.createSession();
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
