0d9f8c3e7ae9da5760f5bb5f824d8cce6b168e2c
clairtonluz <clairton.c.l@gmail.com>
2015-07-03 20:46:54 -0300

    refactoring SRP

--- app/dao/hostDao.js
+++ app/dao/hostDao.js
@@ -4,6 +4,7 @@ var Host = DB.import('host');
 module.exports = {
     save: save,
     findById: findById
+    findAll: findAll
 }
 
 function save(host, callback){
@@ -36,3 +37,71 @@ function getNodes(host, callback){
 	    callback(null, _host);
     });
 }
+
+
+function findAll(callback) {
+	Host.findAll().then(function(hosts){
+		var _hosts = []
+
+		hosts.forEach(function(it, index){
+            getNodes(it, function(err, data){
+                _hosts.push(data);
+
+                if(hosts.length === _hosts.length) {
+                    callback(null, _hosts);
+                }
+            });
+        });
+	}).catch(function(err){
+	    callback(err);
+    });
+
+//  var _hosts = [{
+//	 title: "bytecom-mk-patricia-gomes",
+//	 ip: "10.77.100.1",
+//	 nodes: [
+//			{
+//			  title: "bytecom-patricia-gomes-rep1",
+//			  ip: "10.77.100.101",
+//			  nodes: []
+//			}, {
+//			  title: "bytecom-patricia-gomes-rep2",
+//			  ip: "10.77.100.102",
+//			  nodes: []
+//			}, {
+//			  title: "bytecom-patricia-gomes-rep3",
+//			  ip: "10.77.100.103",
+//			  nodes: []
+//			}, {
+//			  title: "bytecom-patricia-gomes-rep4",
+//			  ip: "10.77.100.104",
+//			  nodes: []
+//			}, {
+//			  title: "bytecom-patricia-gomes-omni",
+//			  ip: "10.77.100.105",
+//			  nodes: []
+//			}]
+//	 }];
+//
+//	var _json = JSON.stringify(_hosts);
+//
+//	res.set('Content-Type', 'application/json');
+//	res.end(_json);
+}
+
+
+//
+//var h1 = {
+//        name: "bytecom-mk-patricia-gomes",
+//        ip: "10.77.100.1"
+//    }
+//var h2 = {
+//        name: "bytecom-patricia-gomes-rep1",
+//        ip: "10.77.100.101"
+//    }
+
+//Host.create(h1).then(function(host){
+//    Host.create(h2).then(function(host2){
+//        host.setNodes([host2])
+//    });
+//});
--- app/routes/hosts.js
+++ app/routes/hosts.js
@@ -10,11 +10,11 @@ module.exports = function(app) {
 }
 
 function save(req, res){
-    Host.create(req.body).then(function(host){
-		httpUtil.sucess(res, host.dataValues);
-	}).catch(function(err){
-		httpUtil.error(res, err);
-	});
+    hostDao.save(req.body, function(err, data) {
+        if (err) httpUtil.error(res, err);
+
+		httpUtil.sucess(res, data);
+    });
 }
 
 function findById(req, res) {
@@ -41,71 +41,9 @@ function getNodes(host, callback){
 
 
 function findAll(req, res) {
-	Host.findAll().then(function(hosts){
-		var _hosts = []
+    hostDao.findAll(function(err, data){
+        if (err) httpUtil.error(res, err);
 
-		hosts.forEach(function(it, index){
-            getNodes(it, function(err, data){
-                _hosts.push(data);
-
-                if(hosts.length === _hosts.length) {
-                    console.log("sucesso");
-                    httpUtil.sucess(res, _hosts);
-                }
-            });
-
-        });
-
-	}).catch(function(err){
-        httpUtil.error(res, err);
+        httpUtil.sucess(res, data);
     });
-
-//  var _hosts = [{
-//	 title: "bytecom-mk-patricia-gomes",
-//	 ip: "10.77.100.1",
-//	 nodes: [
-//			{
-//			  title: "bytecom-patricia-gomes-rep1",
-//			  ip: "10.77.100.101",
-//			  nodes: []
-//			}, {
-//			  title: "bytecom-patricia-gomes-rep2",
-//			  ip: "10.77.100.102",
-//			  nodes: []
-//			}, {
-//			  title: "bytecom-patricia-gomes-rep3",
-//			  ip: "10.77.100.103",
-//			  nodes: []
-//			}, {
-//			  title: "bytecom-patricia-gomes-rep4",
-//			  ip: "10.77.100.104",
-//			  nodes: []
-//			}, {
-//			  title: "bytecom-patricia-gomes-omni",
-//			  ip: "10.77.100.105",
-//			  nodes: []
-//			}]
-//	 }];
-//
-//	var _json = JSON.stringify(_hosts);
-//
-//	res.set('Content-Type', 'application/json');
-//	res.end(_json);
 }
-
-
-//
-//var h1 = {
-//        name: "bytecom-mk-patricia-gomes",
-//        ip: "10.77.100.1"
-//    }
-//var h2 = {
-//        name: "bytecom-patricia-gomes-rep1",
-//        ip: "10.77.100.101"
-//    }
-
-//Host.create(h1).then(function(host){
-//    Host.create(h2).then(function(host2){
-//        host.setNodes([host2])
-//    });
-//});
